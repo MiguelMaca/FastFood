09de7bf5dfa243cb0b0c72c2b17cba4396006625
Jasgu097 <124702997+Jasgu097@users.noreply.github.com>
2023-11-06 23:37:39 -0600

    Add files via upload

--- Menu_Pedidos.py
+++ Menu_Pedidos.py
@@ -63,41 +63,43 @@ modi_pizza=ModificacionPizza()
 modi_bebida=ModificacionBebida()
 modi_complemento=ModificacionComplemento()
 
-pizza=Pizza(1,"Orilla Queso","45",5)
+pizza=Pizza(1,"Orilla Queso",45,5)
 modi_pizza.agregar_pizza(pizza)
-pizza=Pizza(2,"5 carnes","45",5)
+pizza=Pizza(2,"5 carnes",45,5)
 modi_pizza.agregar_pizza(pizza)
-pizza=Pizza(3,"Hawaiana","45",5)
+pizza=Pizza(3,"Hawaiana",45,5)
 modi_pizza.agregar_pizza(pizza)
-pizza=Pizza(4,"Peperoni","45",5)
+pizza=Pizza(4,"Peperoni",45,5)
 modi_pizza.agregar_pizza(pizza)
-pizza=Pizza(5,"Vegetariano","45",5)
+pizza=Pizza(5,"Vegetariano",45,5)
 modi_pizza.agregar_pizza(pizza)
-pizza=Pizza(6,"Jamon","45",5)
+pizza=Pizza(6,"Jamon",45,5)
 modi_pizza.agregar_pizza(pizza)
-pizza=Pizza(7,"Queso","45",5)
+pizza=Pizza(7,"Queso",45,5)
 modi_pizza.agregar_pizza(pizza)
 
-bebida=Bebidas(1,"Coca Cola","5",5)
+bebida=Bebidas(1,"Coca Cola",5,5)
 modi_bebida.agregar_bebida(bebida)
-bebida=Bebidas(2,"Sprite","5",5)
+bebida=Bebidas(2,"Sprite",5,5)
 modi_bebida.agregar_bebida(bebida)
-bebida=Bebidas(3,"Fanta","5",5)
+bebida=Bebidas(3,"Fanta",5,5)
 modi_bebida.agregar_bebida(bebida)
-bebida=Bebidas(4,"Powerade","5",5)
+bebida=Bebidas(4,"Powerade",5,5)
 modi_bebida.agregar_bebida(bebida)
-bebida=Bebidas(5,"Te Lipton","5",5)
+bebida=Bebidas(5,"Te Lipton",5,5)
 modi_bebida.agregar_bebida(bebida)
 
-complemento=Complementos(1,"Parmesan Bites","5",5)
+complemento=Complementos(1,"Parmesan Bites",20,5)
 modi_complemento.agregar_complemento(complemento)
-complemento=Complementos(2,"Chessy Bread","5",5)
+complemento=Complementos(2,"Chessy Bread",20,5)
 modi_complemento.agregar_complemento(complemento)
 
 
 colas=Colas()
 timer=RepeatTimer(22,colas.agregar_cola_listo)
 timer.start()
+
+
 while True:
     print("--Interfaz--")
     print("1. Mostar el menu\n2. Comprar\n3. Pedidos\n4. Salir")
@@ -113,38 +115,65 @@ while True:
         for complemento in modi_complemento.pila_complemento:
             print(f"    {complemento.id}. {complemento.producto}   Precio: {complemento.precio}   Stock: {complemento.stock}")
     elif opcion=="2":
-        print("1. Pizzas\n2. Bebidas\n3. Complementos")
-        compra=input("Que desea agregar a la compra: ")
-        if compra=="1":
-            for pizza in  modi_pizza.pila_pizza:
-                print(f"{pizza.id}. Producto: {pizza.producto}")
-            pizza_comprar=int(input("Que pizza quiere comprar: "))
-            for pizza in  modi_pizza.pila_pizza:
-                if pizza_comprar==pizza.id:
-                    pizza.stock=pizza.stock-1
-                    pedido=pizza.producto,pizza.precio
-                    print(pedido)
-                    colas.agregar_cola_pendiente(pedido)
-        elif compra=="2":
-            for bebida in  modi_bebida.pila_bebida:
-                print(f"{bebida.id}. Producto: {bebida.producto}")
-            bebida_comprar=int(input("Que pizza quiere comprar: "))
-            for bebida in  modi_bebida.pila_bebida:
-                if bebida_comprar==bebida.id:
-                    bebida.stock=bebida.stock-1
-                    pedido=bebida.producto,bebida.precio
-                    print(pedido)
-                    colas.agregar_cola_pendiente(pedido)
-        elif compra=="3":
-            for complemento in  modi_complemento.pila_complemento:
-                print(f"{complemento.id}. Producto: {complemento.producto}")
-            complemento_comprar=int(input("Que pizza quiere comprar: "))
-            for complemento in  modi_complemento.pila_complemento:
-                if complemento_comprar==complemento.id:
-                    complemento.stock=complemento.stock-1
-                    pedido=complemento.producto,complemento.precio
-                    print(pedido)
-                    colas.agregar_cola_pendiente(pedido)
+        Seguir_comprando = False
+        compra_factura=[]
+        total=0
+        while not Seguir_comprando:
+            print("1. Pizzas\n2. Bebidas\n3. Complementos")
+            compra=input("Que desea agregar a la compra: ")
+            if compra=="1":
+                for pizza in  modi_pizza.pila_pizza:
+                    print(f"{pizza.id}. Producto: {pizza.producto}")
+                pizza_comprar=int(input("Que pizza quiere comprar: "))
+                for pizza in  modi_pizza.pila_pizza:
+                    if pizza_comprar==pizza.id:
+                        pizza.stock=pizza.stock-1
+                        pedido=pizza.producto,pizza.precio
+                        print(pedido)
+                        total=total+pizza.precio
+                        compra_factura.append(pedido)
+                        colas.agregar_cola_pendiente(pedido)
+            elif compra=="2":
+                for bebida in  modi_bebida.pila_bebida:
+                    print(f"{bebida.id}. Producto: {bebida.producto}")
+                bebida_comprar=int(input("Que pizza quiere comprar: "))
+                for bebida in  modi_bebida.pila_bebida:
+                    if bebida_comprar==bebida.id:
+                        bebida.stock=bebida.stock-1
+                        pedido=bebida.producto,bebida.precio
+                        print(pedido)
+                        total = total + bebida.precio
+                        compra_factura.append(pedido)
+                        colas.agregar_cola_pendiente(pedido)
+            elif compra=="3":
+                for complemento in  modi_complemento.pila_complemento:
+                    print(f"{complemento.id}. Producto: {complemento.producto}")
+                complemento_comprar=int(input("Que pizza quiere comprar: "))
+                for complemento in  modi_complemento.pila_complemento:
+                    if complemento_comprar==complemento.id:
+                        complemento.stock=complemento.stock-1
+                        pedido=complemento.producto,complemento.precio
+                        print(pedido)
+                        total = total + complemento.precio
+                        compra_factura.append(pedido)
+                        colas.agregar_cola_pendiente(pedido)
+            seguir_comprando=input("1. Si\n2. No\nDesea seguir comprando algun pedido: ")
+            while seguir_comprando!="1" and seguir_comprando !="2":
+                print("Opcion invalida")
+                seguir_comprando = input("1. Si\n2. No\nDesea seguir comprando algun pedido: ")
+            if seguir_comprando=="2":
+                Seguir_comprando=True
+                print(compra_factura)
+                print(total)
+                #Aqui continuas casta
+                #el subtotal es el total que me dio hueva cambiarla
+                #hacer un menu de dos opciones si es cf o con factura
+                #si pide factura hacer otro menu si paga con efectivo o con tarjeta
+                #Si pide codigo de seguridad
+                #pedis nombre, apellido, nit, direccion
+                #poner los datos del cliente en una pila para ofrecer promociones
+                #sacas el iva e imprimis la compra_factura para mostrar la compra del cliente y los totales
+
     elif opcion=="3":
         print("Pedidos pendientes")
         pedido_pendiente=list(colas.cola_pendiente.queue)
